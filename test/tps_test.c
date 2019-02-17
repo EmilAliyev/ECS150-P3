cd43d24fe43180373ccc8a9aae6e4e43b4dc5d35
ealiyev <ealiyev@ad3.ucdavis.edu@pc20.cs.ucdavis.edu>
2019-02-16 17:28:16 -0800

    Created very basic test to make sure tps_create() works

--- test/tps_test.c
+++ test/tps_test.c
@@ -1,6 +1,23 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include "tps.h"
+
+void test_create()
+{
+	printf("Testing tps_create()...\n");
+
+	assert(tps_create() == 0);
+
+	printf("Success!\n");
+}
+
+void test()
+{
+	test_create();
+}
+
 int main()
 {
+	test();
 }

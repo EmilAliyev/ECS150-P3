21ca3198f8b728d1559db2e521ce28808dda8e71
ealiyev <ealiyev@ad3.ucdavis.edu@pc20.cs.ucdavis.edu>
2019-02-16 16:09:16 -0800

    Started commenting in process for tps functions

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -12,23 +12,55 @@
 #include "thread.h"
 #include "tps.h"
 
+/*
+TPS struct
+//Fields:
+//1. Void pointer to memory page
+//2. tid
+*/
+
 /* TODO: Phase 2 */
+
+//Helper functions
 
 int tps_init(int segv)
 {
 	/* TODO: Phase 2 */
+
+	/*
+	Phase 2.1: Do nothing
+	*/
+
 	return 0;
 }
 
 int tps_create(void)
 {
 	/* TODO: Phase 2 */
+
+	/*
+	Phase 2.1
+
+	1. Malloc new tps
+	2. Use mmap to allocate the memory page
+	3. Set tid of tps to current tid
+	4. Add tps to queue
+	*/
+
 	return 0;
 }
 
 int tps_destroy(void)
 {
 	/* TODO: Phase 2 */
+
+	/*
+	Phase 2.1
+
+	1. Find tps for current thread
+	2. Deallocate memory page
+	3. Deallocate tps
+	*/
 	return 0;
 }
 

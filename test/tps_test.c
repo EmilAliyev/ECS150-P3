08c04e9afbb6eed921fd5e7a508287e9f183a153
ealiyev <ealiyev@ad3.ucdavis.edu@pc20.cs.ucdavis.edu>
2019-02-17 11:41:39 -0800

    Retabbed tps_test.c

--- test/tps_test.c
+++ test/tps_test.c
@@ -43,52 +43,52 @@ void test_clone()
 //Init test - must return -1 if called for second time
 void test_init()
 {
-	printf("Testing tps_init()...\n");
+    printf("Testing tps_init()...\n");
 
-	assert(tps_init(1) == -1);
+    assert(tps_init(1) == -1);
 
-	printf("Success!\n");
+    printf("Success!\n");
 }
 
 //Basic destroy test
 void test_destroy()
 {
-	printf("Testing tps_destroy()...\n");
+    printf("Testing tps_destroy()...\n");
 
     //First destroy should be successful
-	assert(tps_destroy() == 0);
+    assert(tps_destroy() == 0);
     
     //Second destroy should be unsuccessful as there is nothing to destroy!
     assert(tps_destroy() == -1);
 
-	printf("Success!\n");
-	
+    printf("Success!\n");
+    
 }
 
 //Basic create test
 void test_create()
 {
-	printf("Testing tps_create()...\n");
+    printf("Testing tps_create()...\n");
 
     //First create should be successful
-	assert(tps_create() == 0);
+    assert(tps_create() == 0);
 
     //Second create should be unsuccessful
     assert(tps_create() == -1);
 
-	printf("Success!\n");
+    printf("Success!\n");
 }
 
 void test()
 {
     test_init();
-	test_create();
-	test_destroy();
+    test_create();
+    test_destroy();
     test_clone();
 }
 
 int main()
 {
     tps_init(1);
-	test();
+    test();
 }
